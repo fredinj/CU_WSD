b2dc2fac4821a8f4560e1c26da3cb2701928449d
Fredin <90355410+fredinj@users.noreply.github.com>
2023-09-23 01:43:49 +0530

    hhh

--- Lab_Ex.8/script.js
+++ Lab_Ex.8/script.js
@@ -1,24 +1,35 @@
 const product_div=document.getElementById("product_div");
+const cat_div=document.getElementById("category");
 const search_form=document.getElementById("search_form");
 const search_content=document.getElementById("search_content");
 
 // Initial products load
 async function loadProducts(){
-    let res = await fetch('https://dummyjson.com/products')
+    let res = await fetch('https://dummyjson.com/products?limit=20')
         .then(products => products.json())
-        .then(products => {
-            products.products.forEach(product => {
+        .then(products_json => {
+            products_json.products.forEach(product => {
                 product_div.innerHTML+=
                 `  
-                <div class="card card_max" style="width: 18rem;">
+                <div class="card" style="width: 18rem;">
                     <img src="${product.thumbnail}" class="card-img-top">
                     <div class="card-body">
                         <h5 class="card-title">${product.title}</h5>
-                        <b><p class="card-text">Price</b>: \$${product.price}</p>
+                        <p class="card-text"><b>Price</b>: \$${product.price}
+                        <br><b>Category</b>: ${product.category}
+                        </p>
                         <p class="card-text">${product.description}</p>
                         </div>
                 </div>
                 `
+            });
+        });
+    
+    res = await fetch('https://dummyjson.com/products/categories')
+        .then(products => products.json())
+        .then(products_json => {
+            products_json.forEach(cat => {
+                cat_div.innerHTML += `<option value="${cat}">${cat}</option>`;
             });
         });
 }
@@ -32,12 +43,17 @@ search_form.addEventListener("submit", async (e)=>{
         let res= await fetch(`https://dummyjson.com/products/search?q=${search_term}`)
             .then(products_raw => products_raw.json())
             .then(products_json =>{
-
+                
                 //sorting   
                 if(e.target.price_sort.checked)
                     products_json.products = products_json.products.sort((a,b) => a.price - b.price);
 
-                if(products_json.total !== 0){
+                //enforcing category in search
+                let cat_value=e.target.category.value;
+                if(cat_value !== "all")
+                    products_json.products = products_json.products.filter((item) => item.category == cat_value);              
+                
+                if(products_json.products.length !== 0){
                     products_json.products.forEach(product => {
                         search_content.innerHTML+=
                         `
